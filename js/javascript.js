ea720ac0f685a72c5c6bbe93d03899cbfec96944
Joe M <joemattar@gmail.com>
2022-08-10 17:49:20 -0400

    Add draw pixels listeners and function

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,27 @@
-const gridDiv = document.querySelector("div.grid");
-const slider = document.querySelector("#resolution")
-const resolutionText = document.querySelector("#resolutiontext");
-
 const gridSize = 800;
+const gridDiv = document.querySelector("div.grid");
+const slider = document.querySelector("#resolution");
+const resolutionText = document.querySelector("#resolutiontext");
+const colorPicker = document.querySelector("#colorpicker");
+
+let mousePressed = false
+let pixels = [];
 
 function createGrid(resolution, size) {
     gridDiv.textContent = "";
     gridDiv.style.height = `${gridSize}px`;
     gridDiv.style.width = `${gridSize}px`;
     for (let i = 1; i <= resolution; i++) {
-        let rowPixelDiv = document.createElement("div");
-        rowPixelDiv.classList.add("pixelRow");
-        rowPixelDiv.style.height = `${size / resolution}px`;
-        gridDiv.appendChild(rowPixelDiv);
+        let rowpixelDiv = document.createElement("div");
+        rowpixelDiv.classList.add("pixelRow");
+        rowpixelDiv.style.height = `${size / resolution}px`;
+        gridDiv.appendChild(rowpixelDiv);
         for  (let j = 1; j <= resolution; j++) {
-            let colPixelDiv = document.createElement("div");
-            colPixelDiv.classList.add("pixel");
-            colPixelDiv.style.width = `${size / resolution}px`;
-            rowPixelDiv.appendChild(colPixelDiv);
+            let pixelDiv = document.createElement("div");
+            pixelDiv.classList.add("pixel");
+            pixelDiv.style.width = `${size / resolution}px`;
+            rowpixelDiv.appendChild(pixelDiv);
+            pixels.push(pixelDiv)
         }
     }
 }
@@ -35,3 +39,15 @@ slider.addEventListener("change", () => {
     resolutionText.textContent = `${slider.value} x ${slider.value}`;
     createGrid(slider.value, gridSize);
 })
+
+addEventListener("mousedown", () => mousePressed = true)
+addEventListener("mouseup", () => mousePressed = false)
+
+addEventListener("mouseover", (e) => {
+    for (let pixel of pixels) {
+        if (pixel.matches(":hover") && mousePressed) {
+            e.target.style.backgroundColor = colorPicker.value;
+        }
+    }
+})
+
