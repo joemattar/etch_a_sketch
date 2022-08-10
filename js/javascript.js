8fbb69124d3b9468fde35af36ee1c8845c2e89fe
Joe M <joemattar@gmail.com>
2022-08-10 15:46:14 -0400

    Create range input for resolution

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,8 @@
-const gridSize = 800;
-let gridResolution = 50;
-
 const gridDiv = document.querySelector("div.grid");
+const slider = document.querySelector("#resolution")
+const resolutionText = document.querySelector("#resolutiontext");
 
+const gridSize = 800;
 
 function createGrid(resolution, size) {
     gridDiv.textContent = "";
@@ -11,16 +11,24 @@ function createGrid(resolution, size) {
     for (let i = 1; i <= resolution; i++) {
         let rowPixelDiv = document.createElement("div");
         rowPixelDiv.classList.add("pixelRow");
-        rowPixelDiv.style.height = `${Math.floor(size / resolution)}px`;
+        rowPixelDiv.style.height = `${size / resolution}px`;
         gridDiv.appendChild(rowPixelDiv);
         for  (let j = 1; j <= resolution; j++) {
             let colPixelDiv = document.createElement("div");
             colPixelDiv.classList.add("pixel");
-            colPixelDiv.style.width = `${Math.floor(size / resolution)}px`;
+            colPixelDiv.style.width = `${size / resolution}px`;
             rowPixelDiv.appendChild(colPixelDiv);
         }
     }
 }
 
+createGrid(slider.value, gridSize);
 
-createGrid(gridResolution, gridSize);
+resolutionText.textContent = `${slider.value} x ${slider.value}`;
+
+
+
+slider.addEventListener("change", () => {
+    resolutionText.textContent = `${slider.value} x ${slider.value}`;
+    createGrid(slider.value, gridSize);
+})
