91c6bce6334a3038a5027be21f3a61558d8e5a8b
Joe M <joemattar@gmail.com>
2022-08-10 18:37:50 -0400

    Add JS reset button event listener

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,10 @@
 const gridSize = 800;
+
 const gridDiv = document.querySelector("div.grid");
 const slider = document.querySelector("#resolution");
 const resolutionText = document.querySelector("#resolutiontext");
 const colorPicker = document.querySelector("#colorpicker");
+const resetButton = document.querySelector("button.reset");
 
 let mousePressed = false
 let pixels = [];
@@ -11,6 +13,7 @@ let pixels = [];
 // Grid creation function
 function createGrid(resolution, size) {
     gridDiv.textContent = "";
+    pixels = [];
     gridDiv.style.height = `${gridSize}px`;
     gridDiv.style.width = `${gridSize}px`;
     for (let i = 1; i <= resolution; i++) {
@@ -23,7 +26,7 @@ function createGrid(resolution, size) {
             pixelDiv.classList.add("pixel");
             pixelDiv.style.width = `${size / resolution}px`;
             rowpixelDiv.appendChild(pixelDiv);
-            pixels.push(pixelDiv)
+            pixels.push(pixelDiv);
         }
     }
 }
@@ -34,20 +37,20 @@ createGrid(slider.value, gridSize);
 // Initial resolution text
 resolutionText.textContent = `${slider.value} x ${slider.value}`;
 
-// Changes slider text
+// Changes slider text on slider sliding only
 slider.addEventListener("input", () => {
     resolutionText.textContent = `${slider.value} x ${slider.value}`;
 })
 
-// Changes slider text and resets grid with new resolution
+// Changes slider text and resets grid with new resolution on slider change
 slider.addEventListener("change", () => {
     resolutionText.textContent = `${slider.value} x ${slider.value}`;
     createGrid(slider.value, gridSize);
 })
 
 // Draws color picker value if mouse is pressed and mouseover
-addEventListener("mousedown", () => mousePressed = true)
-addEventListener("mouseup", () => mousePressed = false)
+addEventListener("mousedown", () => mousePressed = true);
+addEventListener("mouseup", () => mousePressed = false);
 addEventListener("mouseover", (e) => {
     for (let pixel of pixels) {
         if (pixel.matches(":hover") && mousePressed) {
@@ -56,3 +59,8 @@ addEventListener("mouseover", (e) => {
     }
 })
 
+
+// Onb button click resets the grid keeping the current resolution
+resetButton.addEventListener("click", () => {
+    createGrid(slider.value, gridSize);
+});
